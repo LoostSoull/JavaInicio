c906577510754c29e7582b2846874119d0959d56
LoostSoull <lucas.silva.cardoso1996@gmail.com>
2022-07-25 22:18:15 -0300

    atualização, comando basico

---
+++ exercicios/ComandosDeImpressao.java
@@ -0,0 +1,11 @@
+public class ComandosDeImpressao {
+
+    public static void main(String[] args) {
+        
+        //comandos de impressão, %s imprime strings no "pintf" e %d numeros inteiros
+        //%n - quebra linhas, existem diversos =)
+        System.out.printf("%s - %d", "lucas" , 2022);
+        
+        
+    }
+}
